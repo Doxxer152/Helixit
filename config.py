368077763986dac73c96815465a12fcc2ab59904
Doxxer152 <150560657+Doxxer152@users.noreply.github.com>
2024-11-20 20:53:30 +0100

    Add files via upload

---
+++ config.py
@@ -0,0 +1,3 @@
+apihash = '360ce008dc00a5b9db0a4f1015425349'
+apiid = 24175981
+
---
+++ install.sh
@@ -0,0 +1,8 @@
+apt install python
+pip install requests
+pip install telethon
+pip install pystyle
+pip install python-whois
+pip install Faker
+pip install holehe
+pip install phonenumbers
